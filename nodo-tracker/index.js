5523c908377818caeab8c0d6d9b63576a7e7dd04
ZePotente <ezeposada@gmail.com>
2021-10-17 23:06:25 -0300

    funcion de carga de configuracion

---
+++ nodo-tracker/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const configFile = 'config.json'; //se hardcodea el nombre de la config.
+const config = crearConfig(configFile);
+
+function crearConfig(archivo) {
+	const configBuffer = fs.readFileSync(archivo); //por ahora sync porque es lo mismo
+	const configString = configBuffer.toString();
+
+	return JSON.parse(configString);
+}
