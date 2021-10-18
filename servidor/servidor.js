982a385f394bc7bf76852b86951da2eca501b895
ampieroni <agustinapieroni@hotmail.com>
2021-10-18 12:44:06 -0300

    Botonera v1
    Tres botones sin lógica asociada.

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -1,11 +1,13 @@
 var http = require('http');
+var fs = require('fs');
+var botonera = fs.readFileSync('botonera.html')
 var host = 'localhost';
 var puerto = '8080';
 
 var servidor = http.createServer(function(llamar, responder){
     //acá estaria toda la vaina principal
     responder.writeHead(200, {'Content-Type' : 'text/html'});
-    responder.end('<h1>Servidor activo</h1>');
+    responder.write(botonera);
 })
 
 servidor.listen(puerto, host, function(){
