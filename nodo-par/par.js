b968f00345b8138704ea0a315f8a2734847f8a4c
ZePotente <ezeposada@gmail.com>
2021-12-04 16:51:41 -0300

    Par - Server y Cliente UDP

--- nodo-par/par.js
+++ nodo-par/par.js
@@ -12,12 +12,13 @@ function crearConfig(archivo) {
 }
 
 console.log(config);
-
+/*
 //El límite es archivos de medio GiB por la forma de mandarlo.
 // Interfaz D P2P, TCP entre pares
 const net = require('net');
+
 // -- Servidor --
-const puerto = config.puertoTCP;
+const puertoTCP = config.puertoTCP;
 const server = net.createServer(function(socket) {
 	console.log('Se recibio una conexión de otro par.');
 	socket.on('data', function(data) {
@@ -39,10 +40,12 @@ const server = net.createServer(function(socket) {
 	});
 });
 
-server.listen(puerto);
-console.log('Escuchando en el puerto ' + puerto);
+server.listen(puertoTCP);
+console.log('Escuchando en el puerto ' + puertoTCP);
 
 // -- Cliente --
+
+
 const puerto2 = 9999; const ip = '127.0.0.1';
 const client = net.createConnection(puerto2, ip);
 const hash = 'ffffffffffffffffffffffffff';
@@ -75,3 +78,79 @@ client.on('error', function(error) {
 	console.log('Hubo un error en la conexión:');
 	console.log(error);
 });
+*/
+
+// Interfaz C UDP, con el Tracker
+// Creacion del server
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4'); //ipv4
+
+server.on('listening', function() {
+	console.log('Escuchando en el puerto: ' + config.puertoUDP);
+});
+
+server.on('error', function(error) {
+	console.log('Hubo un error:');
+	console.log(error);
+});
+// Respuesta del Tracker
+server.on('message', function(msg, rinfo) {
+	mensajeJSON = armarMensajeJSON(msg);
+	imprimirMensaje(msg, rinfo, mensajeJSON);
+
+	// Llamar a la descarga del archivo (iterando el vector de pares)
+
+});
+server.bind(config.puertoUDP);
+
+// por si hay que hacer algo más que simplemente un toString()
+// si es muy grande y se manda en varios buffers
+function armarMensajeJSON(msg) {
+	mensaje = msg.toString();
+	mensajeJSON = JSON.parse(mensaje);
+	return mensajeJSON;
+}
+
+function imprimirMensaje(msg, rinfo, mensaje) {
+	console.log('Se recibió un mensaje:');
+	console.log('msg');
+	console.log(msg); //objeto JSON
+	console.log('rinfo:');
+	console.log(rinfo);
+	console.log('mensaje:');
+	console.log(mensaje);
+}
+
+// Parte Cliente UDP
+// Solicitud de los pares del archivo .torrente al tracker
+nombreArchTorrente = 'torrentito.torrente';
+// Busca el .torrente en el filesystem,
+torrente = levantarTorrente(nombreArchTorrente);
+console.log(torrente);
+// Prepara y envia la búsqueda
+search = formatoSearch(nextMId(), torrente.hash, torrente.trackerIP, torrente.trackerPort);
+console.log(search);
+
+
+function levantarTorrente(archivo) {
+	const configBuffer = fs.readFileSync(archivo); //por ahora sync porque es lo mismo
+	const configString = configBuffer.toString();
+
+	return JSON.parse(configString);
+}
+
+function nextMId() {
+	return "1";
+}
+
+function formatoSearch(mid, hash, ip, puerto) {
+	let route = '/file' + '/' + hash;
+	
+	return {
+		messageId: mid,
+		route,
+		originIP: ip,
+		originPort: config.puertoUDP,
+		body: {}
+	};
+}
