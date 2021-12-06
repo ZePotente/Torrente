4c2e10ba955ae1bdce172cc38c5dc1ffd0181560
ampieroni <agustinapieroni@hotmail.com>
2021-12-06 18:07:27 -0300

    servidor + html sin funcionalidad

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -1,13 +1,29 @@
 var http = require('http');
 var fs = require('fs');
-var botonera = fs.readFileSync('botonera.html')
+var path = require('path');
+
 var host = 'localhost';
 var puerto = '8080';
 
 var servidor = http.createServer(function(llamar, responder){
     //acá estaria toda la vaina principal
-    responder.writeHead(200, {'Content-Type' : 'text/html'});
-    responder.write(botonera);
+
+    if(req.url =='/'){
+        fs.readFile('index.html','utf-8',(err,html)=>{
+            responder.writeHead(200, {'Content-Type' : 'text/html'});
+            responder.write(html);
+        });
+    }else if(req.url.match(/.css$/)){
+        const reqPath = path.join(dirname,'servidor',req.url);
+        const fileStream = fs.createReadStream(reqPath, 'utf-8');
+
+        responder.writeHead(200, {'Content-Type' : 'text/css'});
+        fileStream.pipe(responder);
+    }
+    else{
+        responder.writeHead(404, {'Content-Type' : 'text/plain'});
+        responder.write('404 ERROR');
+    }
 })
 
 servidor.listen(puerto, host, function(){
